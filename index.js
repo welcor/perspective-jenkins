f721df226c9dff68a2000e5c63bfdda7ce3b8947
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-10-21 13:42:02 +0200

    Created a repo, service and resource for jenkins

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-module.exports = function(api) {
+var JenkinsRepository = require('./lib/jenkinsRepository');
+var JenkinsService = require('./lib/jenkinsService');
+var jenkinsResource = require('./lib/jenkinsResource');
 
-  console.log(api);
+module.exports = function(api, config) {
 
   return {
-    setup: function() {}
+    setup: function() {
+      var repo = new JenkinsRepository(config);
+      var service = new JenkinsService(repo);
+      new jenkinsResource(api.webSocketServer, api.restServer, service);
+    }
   };
 
 };
