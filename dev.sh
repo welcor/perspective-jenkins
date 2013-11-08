f6f75bf89e3e7f594eac7edc2d18e7852d44e3d9
Thomas Arp <thomas.arp@bekk.no>
2013-11-08 15:21:33 +0100

    added comments

--- dev.sh
+++ dev.sh
@@ -1,1 +1,7 @@
+# Variables set up by this script:
+# SERVER_PORT            Port exposed by this component.
+# JENKINS_USERNAME       Username for jenkins instance
+# JENKINS_URL            URL to jenkins frontpage
+# SERVER_ALLOWED_ORIGIN  Allowed adress for contacting this server.
+
  SERVER_PORT=8888 JENKINS_USERNAME="hei" JENKINS_URL="http://localhost:9999" SERVER_ALLOWED_ORIGIN="http://localhost:8000" nodemon index.js
